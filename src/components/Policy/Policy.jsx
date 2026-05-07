188b2a5d1dee680972107fec9a6f1123431b0967
Behruz Jumanov <behruzjumano55@gmail.com>
2026-05-07 13:06:09 +0500

    added new files

--- src/components/Policy/Policy.jsx
+++ src/components/Policy/Policy.jsx
@@ -1,6 +1,6 @@
 
 export default function Policy() {
   return (
-    <div>sdjwedjnwjewelwae</div>
+    <div></div>
   )
 }
--- src/pages/Policy/Page.jsx
+++ src/pages/Policy/Page.jsx
@@ -4,27 +4,37 @@ import Policy from "../../components/Policy/Policy"
 function Page() {
   return (
     <>
-<section>
-    <div>
-        <h2 className="text-[48px] font-semibold">
+<section className="">
+    <div className="mt-[84px]  pl-[272px]">
+        <h2 className="text-[48px] font-semibold ">
             Privacy Policy
         </h2>
-        <p className="text-[16px] leading-[24px] text-[#232536]">Last Updated  on 27th January 2021</p>
+        <p className="text-[16px] pt-[8px] leading-[24px] text-[#232536]">Last Updated  on 27th January 2021</p>
     </div>
 
-    <div className="bg-[#ECF8F9] w-[1280px] h-[1280px]  justify-center">
-     <div>
-        <h3 className="">Transform Your Idea Into Reality with Ether a Leading Digital Agency</h3>
-        <p>Writing UX copies can be a little frustrating and confusing, and sometimes we are unsure about how to get the right word. To crack the code for the UX copies, we at Zeta Design wanted to build a Figma plugin for the larger design community. The plugin is called the Ghost UXWriter and has a set of UX copies cataloged and categorized with a voice and tone variation ranging from plain, casual to playful. The intention to build this Figma plugin originated from our Medium blog post, ‘Designing voice and tone for error messages.</p>
-        <p>Writing UX copies can be a little frustrating and confusing, and sometimes we are unsure about how to get the right word. To crack the code for the UX copies, we at Zeta Design wanted to build a Figma plugin for the larger design community. The plugin is called the Ghost UXWriter and has a set of UX copies cataloged and categorized with a voice and tone variation ranging from plain, casual to playful. The intention to build this Figma plugin originated from our Medium blog post, ‘Designing voice and tone for error messages.</p>
-        <p>Writing UX copies can be a little frustrating and confusing, and sometimes we are unsure about how to get the right word. To crack the code for the UX copies, we at Zeta Design wanted to build a Figma plugin for the larger design community. The plugin is called the Ghost UXWriter and has a set of UX copies cataloged and categorized with a voice and tone variation ranging from plain, casual to playful. The intention to build this Figma plugin originated from our Medium blog post, ‘Designing voice and tone for error messages.</p>
-        <p>Writing UX copies can be a little frustrating and confusing, and sometimes we are unsure about how to get the right word. To crack the code for the UX copies, we at Zeta Design wanted to build a Figma plugin for the larger design community. The plugin is called the Ghost UXWriter and has a set of UX copies cataloged and categorized with a voice and tone variation ranging from plain, casual to playful. The intention to build this Figma plugin originated from our Medium blog post, ‘Designing voice and tone for error messages.</p>
+    <div className="bg-[#ECF8F9] w-[1280px] h-[1280px]  justify-center px-[221px] mx-[160px] mt-[84px] pt-[96px]">
+     <div className="">
+        <h3 className="text-[36px] font-semibold leading-[54px]">Transform Your Idea Into Reality with Ether a <br /> Leading Digital Agency</h3>
+        <p className="pt-[24px]">Writing UX copies can be a little frustrating and confusing, and sometimes we are unsure about how to <br /> get the right word. To crack the code for the UX copies, we at Zeta Design wanted to build a Figma plugin <br /> for the larger design community. The plugin is called the Ghost UXWriter and has a set of UX copies <br /> cataloged and categorized with a voice and tone variation ranging from plain, casual to playful. The <br /> intention to build this Figma plugin originated from our Medium blog post, ‘Designing voice and tone for <br /> error messages.</p>
+
+        <p className="pt-[24px]">Writing UX copies can be a little frustrating and confusing, and sometimes we are unsure about how to <br /> get the right word. To crack the code for the UX copies, we at Zeta Design wanted to build a Figma plugin <br /> for the larger design community. The plugin is called the Ghost UXWriter and has a set of UX copies <br /> cataloged.</p>
+
+        <p className="pt-[24px]">Writing UX copies can be a little frustrating and confusing, and sometimes we are unsure about how to <br /> get the right word. To crack the code for the UX copies, we at Zeta Design wanted to build a Figma plugin <br /> for the larger design community. The plugin is called the Ghost UXWriter and has a set of UX copies <br /> cataloged.</p>
+
+        <p className="pt-[24px]">Writing UX copies can be a little frustrating and confusing, and sometimes we are unsure about how to <br /> get the right word. To crack the code for the UX copies, we at Zeta Design wanted to build a Figma plugin <br /> for the larger design community. The plugin is called the Ghost UXWriter and has a set of UX copies <br /> cataloged and categorized with a voice and tone variation ranging from plain, casual to playful. The <br /> intention to build this Figma plugin originated from our Medium blog post, ‘Designing voice.</p>
      </div>
-     <div>
-        <h4>We disclose your information in the following manner:</h4>
-        <p>Business partners, suppliers and sub-contractors (“Affiliates”): Affiliates may use this information to help provide, understand, and improve our services and Affiliate’s own services for the performance of any contract we enter into </p>
-        <p>Business partners, suppliers and sub-contractors (“Affiliates”): Affiliates may use this information to help provide, understand, and improve our services and Affiliate’s own services for the performance of any contract we enter into </p>
-        <p>Business partners, suppliers and sub-contractors (“Affiliates”): Affiliates may use this information to help provide, understand, and improve our services and Affiliate’s own services for the performance of any contract we enter into </p>
+     <div className="mt-[40px]">
+        <h4 className="text-[24px] font-semibold leading-[36px]">We disclose your information in the following manner:</h4>
+        <p className="pt-[16px]">Business partners, suppliers and sub-contractors (“Affiliates”): Affiliates may use this information to help <br /> provide, understand, and improve our services and Affiliate’s own services for the performance of any <br /> contract we enter into </p>
+
+        <p className="pt-[30px]">Financial Institutions and Auditors: In order to complete third party financial, technical and legal audits of <br /> our operations to help us operate our business, we may need to share your information with financial <br /> institutions and auditors.</p>
+
+        <p className="pt-[30px]">Advertisers and Advertising Networks: To provide you with select and serv e relevant advertisements to <br /> you and others, we may share your data with advertiser sand advertising networks. We do not disclose <br /> your Personal Information or Sensitive Personal Information to our advertisers, but we may provide them <br /> with aggregate information about our users (for example, we may inform them that any given number of <br /> participants in a specified age group who clicked on their advertisement on any given day). We may also <br /> use such aggregate information to help advertisers reach the kind of audience they want to target.</p>
+     </div>
+     <div className="w-[296px] h-[24px]">
+    <div className="w-[30px] bg-blue-500"></div>
+    <div className="w-[184px] bg-[#FFD3AF]"></div>
+    <div></div>
      </div>
     </div>
 </section>
