55c09c9e1c6b37ae931fd9a94afe020bfe242413
Behruz Jumanov <behruzjumano55@gmail.com>
2026-05-09 15:46:40 +0500

    Fixed the errors

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,56 +1,56 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayouts/MainLayout";
-import { Home } from "../pages/Home/Home"; 
+import { Home } from "../pages/Home/Home";
 import { Company } from "../pages/Home/Company";
 import { Service } from "../pages/Home/Service";
 import { Teamtemp } from "../pages/Home/Teamtemp";
 import Career from "../pages/Home/Career/Career";
-import  { Blog }  from "../pages/Home/Blog";
-import  Policy   from "../pages/Policy"
-import  { InnerPage }  from "../pages/Home/InnerPage";
+import { Blog } from "../pages/Home/Blog";
+import { Policy } from "../pages/Policy";
+import { InnerPage } from "../pages/Home/InnerPage";
 import CareerPage from "../pages/CareerPage/Page";
 
 export const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<MainLayout />,
-        children: [
-            {
-          index:true,
-          element:<Home />
-            },
-            {
-                path:"/company",
-                element:<Company />
-            },
-            {
-                path:"/service",
-                element:<Service />
-            },
-            {
-                path:"/teamtemp",
-                element:<Teamtemp />
-            },
-            {
-                path:"/Career",
-                element:<Career />
-            },
-            {
-                path:"/blog",
-                element:<Blog />
-            },
-            {
-                path:"/policy",
-                element:<Policy />
-            },
-            {
-                path:"/innerpage",
-                element:<InnerPage />
-            },
-            {
-                path:"/inner",
-                element:<CareerPage />
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/company",
+        element: <Company />,
+      },
+      {
+        path: "/service",
+        element: <Service />,
+      },
+      {
+        path: "/teamtemp",
+        element: <Teamtemp />,
+      },
+      {
+        path: "/Career",
+        element: <Career />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+      {
+        path: "/policy",
+        element: <Policy />,
+      },
+      {
+        path: "/innerpage",
+        element: <InnerPage />,
+      },
+      {
+        path: "/inner",
+        element: <CareerPage />,
+      },
+    ],
+  },
+]);
